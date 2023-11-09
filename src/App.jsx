90be21aad4573c1e21466dd1e49277d278b2f81e
Jake1388 <jaknobb73@gmail.com>
2023-11-09 17:40:05 +0000

    Fixed issue with certain pages not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
+import AfterLogin from "./pages/AfterLogin";
+import Profile from "./pages/Profile";
+import ArticlesAfterLogin from "./pages/ArticlesAfterLogin";
 
 function App() {
   return (
@@ -7,6 +10,12 @@ function App() {
       <div className='container'>
         <Routes>
           <Route path='' element={<HomePage />}></Route>
+          <Route path='/AfterLogin' element={<AfterLogin />}></Route>
+          <Route path='/Profile' element={<Profile />}></Route>
+          <Route path='/ArticlesAfterLogin' element={<ArticlesAfterLogin />}></Route>
+
+
+
         </Routes>
       </div>
     </>
--- src/pages/ArticlesAfterLogin.jsx
+++ src/pages/ArticlesAfterLogin.jsx
@@ -1,10 +1,13 @@
 import NavbarAfterLogin from "../components/NavbarAfterLogin";
 import ArticlesAfterLogin from "../components/ArticlesAfterLogin";
+import Footer from "../components/Footer"
+
 export default function ArticlesAfterLogin() {
   return (
     <>
       <NavbarAfterLogin />
       <ArticlesAfterLogin />
+      <Footer />
     </>
   );
 }
